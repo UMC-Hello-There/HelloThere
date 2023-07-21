2b1aba48f9aa3ffe94e7d29e4a8dd596dea28f5d
duddn2012 <31121731+duddn2012@users.noreply.github.com>
2023-07-21 17:05:19 +0900

    Point 처리 (#30)
    * House Save 시 Point 처리
    
    * #16 House Save 시 Point 처리

--- src/main/java/com/example/hello_there/house/HouseController.java
+++ src/main/java/com/example/hello_there/house/HouseController.java
@@ -1,12 +1,17 @@
 package com.example.hello_there.house;
 
+import com.example.hello_there.house.dto.GetHouseRes;
 import com.example.hello_there.house.dto.PatchHouseReq;
 import com.example.hello_there.house.dto.PostHouseReq;
 import com.example.hello_there.exception.BaseException;
 import com.example.hello_there.exception.BaseResponse;
+import com.example.hello_there.house.vo.GeoPoint;
 import com.example.hello_there.login.jwt.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,5 +43,14 @@ public class HouseController {
         House house = houseService.createHouse(postHouseReq);
         return new BaseResponse<>(house.getCity() + " " + house.getDistrict() +
                 " " + house.getHouseName() + " 등록이 완료되었습니다.");
+    }
+
+    /** GPS 위치 인증을 위한 근처 아파트 조회 **/
+    @GetMapping("/spatial/radius")
+    public BaseResponse<List<GetHouseRes>> findByRadius(@RequestParam("lng") Double lng, @RequestParam("lat") Double lat, @RequestParam("radius") Double radius) {
+        List<House> houseList = houseService.findByRadius(new GeoPoint(lng, lat), radius);
+        List<GetHouseRes> res =
+                houseList.stream().map(it-> GetHouseRes.mapEntityToResponse(it)).collect(Collectors.toList());
+        return new BaseResponse<>(res);
     }
 }
