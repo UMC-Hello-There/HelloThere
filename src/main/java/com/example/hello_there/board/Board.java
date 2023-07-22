04c1e61ea776278239e7c19225e758e91197e4f5
이다빈 <dlekqls6204@naver.com>
2023-07-23 02:51:15 +0900

    [#39] LikeBoard 테이블 생성 및 관계매핑

--- src/main/java/com/example/hello_there/board/Board.java
+++ src/main/java/com/example/hello_there/board/Board.java
@@ -1,5 +1,6 @@
 package com.example.hello_there.board;
 
+import com.example.hello_there.board.like.LikeBoard;
 import com.example.hello_there.board.photo.PostPhoto;
 import com.example.hello_there.user.User;
 import com.example.hello_there.utils.BaseTimeEntity;
@@ -42,6 +43,10 @@ public class Board extends BaseTimeEntity {
     // 게시 사진과 관계매핑
     @OneToMany(mappedBy = "board", cascade = {CascadeType.ALL}, orphanRemoval = true)
     private List<PostPhoto> photoList = new ArrayList<>();
+
+    // 게시글 좋아요와 관계매핑
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    private List<LikeBoard> likeBoards = new ArrayList<>();
 
     public void updateBoard(BoardType boardType, String title, String content){
         this.boardType = boardType;
---
+++ src/main/java/com/example/hello_there/board/like/LikeBoardRepository.java
@@ -0,0 +1,7 @@
+package com.example.hello_there.board.like;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeBoardRepository extends JpaRepository<LikeBoard, Long> {
+
+}
