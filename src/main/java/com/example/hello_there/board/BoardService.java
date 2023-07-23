023337166de8292addbd92e25256007bff6fa055
gmlstjq123 <133740938+gmlstjq123@users.noreply.github.com>
2023-07-23 20:13:01 +0900

    Hello there 49 (#50)
    * [#37] 조회수 컬럼 추가 및 관련 코드 수정
    
    * [#37] board 카테고리별 전체 조회 추가
    
    * [#37] BoardController uri 수정
    
    * [#37] 게시글 부분/전체 조회시 댓글수 반환 추가
    
    * HelloThere-40 update user endpoint
    
    * HelloThere-44 update user api
    
    * HelloThere-44 update user api
    
    * HelloThere-47 resolve conflict
    
    * HelloThere-47 resolve conflict
    
    ---------
    
    Co-authored-by: 이다빈 <dlekqls6204@naver.com>

--- src/main/java/com/example/hello_there/board/BoardService.java
+++ src/main/java/com/example/hello_there/board/BoardService.java
@@ -15,15 +15,7 @@ import com.example.hello_there.report.Report;
 import com.example.hello_there.report.ReportRepository;
 import com.example.hello_there.report.ReportService;
 import com.example.hello_there.user.User;
-import com.example.hello_there.user.UserService;
-import com.example.hello_there.user.UserStatus;
-import com.example.hello_there.report.Report;
-import com.example.hello_there.report.ReportRepository;
-import com.example.hello_there.report.ReportService;
-import com.example.hello_there.user.User;
 import com.example.hello_there.user.UserRepository;
-
-import com.example.hello_there.user.UserService;
 import com.example.hello_there.user.UserStatus;
 import com.example.hello_there.utils.S3Service;
 import com.example.hello_there.utils.UtilService;
@@ -45,7 +37,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -307,4 +298,4 @@ public class BoardService {
 
         return "게시글 작성자에 대한 신고 처리가 완료되었습니다.";
     }
-
+}
