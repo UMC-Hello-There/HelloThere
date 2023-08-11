95bbb3cc24860014e7b0fbd63e36bd40b8011c16
gmlstjq123 <gmlstjq123@naver.com>
2023-08-11 17:20:25 +0900

    HelloThere-108 빈 닉네임 예외처리 및 PostBoardReq의 HouseId 삭제

--- src/main/java/com/example/hello_there/user/UserService.java
+++ src/main/java/com/example/hello_there/user/UserService.java
@@ -191,27 +191,11 @@ public class UserService {
     }
 
     /**
-     * 모든 아파트 주민 조회
+     * 본인의 닉네임과 프로필 사진 조회
      */
-    public List<GetUserRes> getMembers(Long userId) throws BaseException {
+    public GetUserRes getUsersByNickname(Long userId) throws BaseException{
         User user = utilService.findByUserIdWithValidation(userId);
-        List<User> users = userRepository.findUsersByHouseId(user.getHouse().getHouseId());
-        return users.stream()
-                .map(GetUserRes::new) // 여기서 생성자를 활용하여 User 객체를 GetUserRes로 매핑
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * 특정 유저를 닉네임으로 조회
-     */
-    public List<GetUserRes> getUsersByNickname(String nickname) throws BaseException{
-        List<User> users = userRepository.findUserByNickName(nickname);
-        if(users.isEmpty()) {
-            throw new BaseException(NONE_EXIST_NICKNAME);
-        }
-        return users.stream()
-                .map(GetUserRes::new) // 여기서 생성자를 활용하여 User 객체를 GetUserRes로 매핑
-                .collect(Collectors.toList());
+        return new GetUserRes(user); // 생성자를 활용하여 User 객체를 GetUserRes로 매핑
     }
 
     /**
