d24c5cbf83e20a312a0a9899845d6e35039ec8c7
gmlstjq123 <gmlstjq123@naver.com>
2023-08-17 19:59:31 +0900

    HelloThere-129 CI/CD 테스트2

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+BUILD_JAR=$(ls /home/ubuntu/.ssh/HelloThere/build/libs/*.jar | tail -n 1)
 REPOSITORY=/home/ubuntu/.ssh/HelloThere
 cd $REPOSITORY
 
@@ -7,20 +7,21 @@ APP_NAME=hello_there-0.0.1-SNAPSHOT.jar
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
+DEPLOY_PATH=/home/.ssh/HelloThere/
+cp $BUILD_JAR $DEPLOY_PATH
+
 CURRENT_PID=$(pgrep -f $APP_NAME)
-cp /home/ubuntu/.ssh/HelloThere/src/main/resources/application.yml /home/ubuntu/application.yml2
+
 if [ -z $CURRENT_PID ]
 then
   echo "> 종료할 애플리케이션이 없습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-cp /home/ubuntu/application.yml2 /home/ubuntu/.ssh/HelloThere/src/main/resources/application.yml
-./gradlew build
-sleep 100
-
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+chmod +x $DEPLOY_JAR
 echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
