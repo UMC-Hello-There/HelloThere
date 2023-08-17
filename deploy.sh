31219718938363b251f60c585aba7f52302cb157
gmlstjq123 <gmlstjq123@naver.com>
2023-08-18 01:43:30 +0900

    HelloThere-129 CI/CD 테스트2

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,9 @@ APP_NAME=hello_there-0.0.1-SNAPSHOT.jar
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1) # hello_there-0.0.1-SNAPSHOT.jar
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME # /home/ubuntu/.ssh/HelloThere/build/libs/hello_there-0.0.1-SNAPSHOT.jar
 
+DEPLOY_PATH=/home/ubuntu/.ssh/HelloThere/
+cp $BUILD_JAR $DEPLOY_PATH # cp /home/ubuntu/.ssh/HelloThere/build/libs/hello_there-0.0.1-SNAPSHOT.jar /home/ubuntu/.ssh/HelloThere/
+
 CURRENT_PID=$(pgrep -f $APP_NAME) # 기존 서버 프로세스의 pid
 
 if [ -z $CURRENT_PID ] # current pid의 길이가 zero이면(= 기존 프로세스가 없으면)
@@ -19,7 +22,7 @@ else
   sleep 5
 fi
 
-DEPLOY_JAR=$REPOSITORY/build/libs$JAR_NAME # /home/ubuntu/.ssh/HelloThere/hello_there-0.0.1-SNAPSHOT.jar
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME # /home/ubuntu/.ssh/HelloThere/hello_there-0.0.1-SNAPSHOT.jar
 chmod +x $DEPLOY_JAR
 echo "> Deploy - $JAR_PATH "
 nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
