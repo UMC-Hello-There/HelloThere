af255dd5d2b2577481883dd5c71f0fd5d5d7a82e
gmlstjq123 <gmlstjq123@naver.com>
2023-08-17 18:54:59 +0900

    HelloThere-129 CI/CD 테스트2

--- deploy.sh
+++ deploy.sh
@@ -9,21 +9,6 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-# 기존 yml 파일 백업 및 복원 함수 정의
-backup_and_restore_yml() {
-  # 백업 파일 이름 생성 (타임스탬프 활용)
-  TIMESTAMP=$(date +%Y%m%d%H%M%S)
-  BACKUP_YML_PATH=/home/ubuntu/.ssh/HelloThere/src/main/resources/application_$TIMESTAMP.yml
-
-  # 기존 yml 파일 백업
-  cp /home/ubuntu/.ssh/HelloThere/src/main/resources/application.yml $BACKUP_YML_PATH
-
-  # 서버 재시작 이후에 복원
-  echo "> 서버 재시작 이전 기존 yml 파일 백업: $BACKUP_YML_PATH"
-  echo "> 기존 yml 파일을 서버 재시작 후에 복원합니다."
-  cp $BACKUP_YML_PATH /home/ubuntu/.ssh/HelloThere/src/main/resources/application.yml
-}
-
 if [ -z $CURRENT_PID ]
 then
   echo "> 종료할 애플리케이션이 없습니다."
@@ -32,10 +17,6 @@ else
   kill -15 $CURRENT_PID
   sleep 5
 fi
-
-# 기존 yml 파일 백업
-backup_and_restore_ymlls
-
 
 echo "> Deploy - $JAR_PATH "
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
