5fed6f157c51e3e52dc28a5c88204bcee5021b30
gmlstjq123 <gmlstjq123@naver.com>
2023-08-18 02:01:14 +0900

    HelloThere-129 CI/CD 테스트2

--- src/main/java/com/example/hello_there/user/UserController.java
+++ src/main/java/com/example/hello_there/user/UserController.java
@@ -42,7 +42,7 @@ public class UserController {
     /**
      * CI/CD의 정상 동작을 확인하는 API
      */
-    @GetMapping("/cd-test")
+    @GetMapping("/cd-check")
     public String cdcheck() {
         return "OK";
     }
