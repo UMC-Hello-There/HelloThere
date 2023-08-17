459b4bd0502b0d27b2d26365b004f5785ca23836
gmlstjq123 <133740938+gmlstjq123@users.noreply.github.com>
2023-08-17 11:20:02 +0900

    Delete Secret.java

--- src/main/java/com/example/hello_there/utils/Secret.java
+++
@@ -1,9 +0,0 @@
-package com.example.hello_there.utils;
-
-import org.springframework.beans.factory.annotation.Value;
-
-public class Secret {
-    public static String JWT_SECRET_KEY = "135b8378904571a649516713c9b3bbffc14f3464a3131504aec324cde5327b4d";
-    public static String USER_INFO_PASSWORD_KEY = "49e1b884c9469230ae83cd13ff41a03edcfba4288ba75be7ad336c6d8e88d249";
-    public static String ROOM_PASSWORD_KEY = "4ff7e6d380cd64c20d7b3c5d60263f5c3a69e2b8457e496e551a93e5f81923df";
-}
