f4c2a9e5e6373bb9f0724d4603b36b5d88da9268
gmlstjq123 <gmlstjq123@naver.com>
2023-08-17 16:57:14 +0900

    HelloThere-129 CI/CD 테스트2

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/.ssh/HelloThere
 cd $REPOSITORY
 
-APP_NAME=hello-there
+APP_NAME=hello_there
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
