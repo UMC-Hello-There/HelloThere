cc559d595df741823f49a78c329ec7d6e1b91d36
gmlstjq123 <gmlstjq123@naver.com>
2023-08-21 13:15:07 +0900

    HelloThere-131 Auto Scaling 그룹에 코드 배포가 잘 적용되는지 확인

--- src/main/java/com/example/hello_there/login/jwt/JwtProvider.java
+++ src/main/java/com/example/hello_there/login/jwt/JwtProvider.java
@@ -15,8 +15,8 @@ import java.util.Date;
 @Slf4j
 @Component
 public class JwtProvider {
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1 * 24 * 60 * 60 * 1000L; //refreshToken 유효기간 1일
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1 * 60 * 60 * 1000L; //accessToken 유효기간 1시간
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 3 * 24 * 60 * 60 * 1000L; //refreshToken 유효기간 3일
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 12 * 60 * 60 * 1000L; //accessToken 유효기간 12시간
 
     // private static final long REFRESH_TOKEN_EXPIRE_TIME = 60 * 1000L; //refreshToken 유효기간 1분,  refrshToken 테스트를 위해 사용
     // private static final long ACCESS_TOKEN_EXPIRE_TIME = 10 * 1000L; //유효기간 10초, refrshToken 테스트를 위해 사용
